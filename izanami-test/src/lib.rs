f2a05e22b50dc1941fd5e14d8db4d10a11be52fe
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2019-02-03 19:58:54 +0900

    izanami-test: add doctest

--- izanami-test/src/lib.rs
+++ izanami-test/src/lib.rs
@@ -1,4 +1,73 @@
 //! The basic utility for testing HTTP services.
+//!
+//! The purpose of this crate is to provide the way to test HTTP services
+//! used by `izanami` server without using the low level I/O.
+//!
+//! # Example
+//!
+//! ```
+//! # #![deny(deprecated)]
+//! use {
+//!     http::{Request, Response},
+//!     izanami_service::MakeService,
+//!     izanami_test::Server,
+//! };
+//! # use {
+//! #   izanami_service::Service,
+//! #   std::io,
+//! # };
+//!
+//! # fn test_echo() -> izanami_test::Result<()> {
+//! // the target service factory to be tested.
+//! let make_service = {
+//!     struct Echo(());
+//!
+//!     impl<Ctx, Bd> MakeService<Ctx, Request<Bd>> for Echo {
+//!         // ...
+//! #       type Response = Response<String>;
+//! #       type Error = io::Error;
+//! #       type Service = Echo;
+//! #       type MakeError = io::Error;
+//! #       type Future = futures::future::FutureResult<Self::Service, Self::MakeError>;
+//! #       fn make_service(&self, _: Ctx) -> Self::Future { futures::future::ok(Echo(())) }
+//!     }
+//! #   impl<Bd> Service<Request<Bd>> for Echo {
+//! #       type Response = Response<String>;
+//! #       type Error = io::Error;
+//! #       type Future = futures::future::FutureResult<Self::Response, Self::Error>;
+//! #       fn poll_ready(&mut self) -> futures::Poll<(), Self::Error> { Ok(().into()) }
+//! #       fn call(&mut self, _: Request<Bd>) -> Self::Future { futures::future::ok(Response::new("hello".to_string())) }
+//! #   }
+//!
+//!     Echo(())
+//! };
+//!
+//! // create a `Server`, that contains the specified
+//! // service factory and a runtime for driving the inner
+//! // asynchronous tasks.
+//! let mut server = Server::new(make_service)?;
+//!
+//! // create a `Client` to test an established connection
+//! // with the peer.
+//! let mut client = server.client()?;
+//!
+//! // applies an HTTP request to the client and await
+//! // its response.
+//! //
+//! // the method simulates the behavior of service until
+//! // just before starting to send the response body.
+//! let response = client.respond(
+//!     Request::get("/").body(())?
+//! )?;
+//! assert_eq!(response.status(), 200);
+//!
+//! // drive the response body and await its result.
+//! let body = response.send_body()?;
+//! assert_eq!(body.to_utf8()?, "hello");
+//! # Ok(())
+//! # }
+//! # fn main() {}
+//! ```
 
 #![doc(html_root_url = "https://docs.rs/izanami-test/0.1.0-preview.1")]
 #![deny(
