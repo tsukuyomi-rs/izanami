a103048814542b5efd5c64012670f1ecd8a4e14a
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2019-02-08 03:25:23 +0900

    hide implementation details exposed from RequestBody

--- src/service.rs
+++ src/service.rs
@@ -1,6 +1,6 @@
 use {
     crate::error::BoxedStdError,
-    bytes::Buf,
+    bytes::{Buf, BufMut, Bytes},
     futures::{Future, Poll},
     http::{HeaderMap, Request, Response},
     hyper::body::Payload as _Payload,
@@ -9,39 +9,76 @@ use {
         buf_stream::{BufStream, SizeHint},
         http::{HasTrailers, Upgrade},
     },
-    std::marker::PhantomData,
+    std::{io, marker::PhantomData},
+    tokio::io::{AsyncRead, AsyncWrite},
 };
 
-/// A struct that represents the stream of chunks from client.
+fn already_upgraded() -> BoxedStdError {
+    failure::format_err!("the request body has already been upgraded")
+        .compat()
+        .into()
+}
+
+/// An asynchronous stream of chunks that represents the HTTP request body.
 #[derive(Debug)]
 pub struct RequestBody(Inner);
 
 #[derive(Debug)]
 enum Inner {
-    Raw(hyper::Body),
+    Stream(hyper::Body),
     OnUpgrade(hyper::upgrade::OnUpgrade),
 }
 
 impl RequestBody {
     pub(crate) fn from_hyp(body: hyper::Body) -> Self {
-        RequestBody(Inner::Raw(body))
+        RequestBody(Inner::Stream(body))
+    }
+
+    /// Returns whether the body is complete or not.
+    pub fn is_end_stream(&self) -> bool {
+        match &self.0 {
+            Inner::Stream(body) => body.is_end_stream(),
+            _ => true,
+        }
+    }
+
+    /// Returns whether this stream has already been upgraded.
+    ///
+    /// If this method returns `true`, the result from `BufStream::poll_buf`
+    /// or `HasTrailers::poll_trailers` becomes an error.
+    pub fn is_upgraded(&self) -> bool {
+        match self.0 {
+            Inner::OnUpgrade(..) => true,
+            _ => false,
+        }
+    }
+
+    /// Returns a length of the total bytes, if possible.
+    pub fn content_length(&self) -> Option<u64> {
+        match &self.0 {
+            Inner::Stream(body) => body.content_length(),
+            _ => None,
+        }
     }
 }
 
 impl BufStream for RequestBody {
-    type Item = hyper::Chunk;
-    type Error = hyper::Error;
+    type Item = io::Cursor<Bytes>;
+    type Error = BoxedStdError;
 
     fn poll_buf(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
         match &mut self.0 {
-            Inner::Raw(body) => body.poll_data(),
-            Inner::OnUpgrade(..) => panic!("the request body has already been upgraded"),
+            Inner::Stream(body) => body
+                .poll_data()
+                .map(|x| x.map(|opt| opt.map(|chunk| io::Cursor::new(chunk.into_bytes()))))
+                .map_err(Into::into),
+            Inner::OnUpgrade(..) => Err(already_upgraded()),
         }
     }
 
     fn size_hint(&self) -> SizeHint {
         match &self.0 {
-            Inner::Raw(body) => {
+            Inner::Stream(body) => {
                 let mut hint = SizeHint::new();
                 if let Some(len) = body.content_length() {
                     hint.set_upper(len);
@@ -49,36 +86,100 @@ impl BufStream for RequestBody {
                 }
                 hint
             }
-            Inner::OnUpgrade(..) => panic!("the request body has already been upgraded"),
+            Inner::OnUpgrade(..) => SizeHint::new(),
         }
     }
 }
 
 impl HasTrailers for RequestBody {
-    type TrailersError = hyper::Error;
+    type TrailersError = BoxedStdError;
 
     fn poll_trailers(&mut self) -> Poll<Option<HeaderMap>, Self::TrailersError> {
         match &mut self.0 {
-            Inner::Raw(body) => body.poll_trailers(),
-            Inner::OnUpgrade(..) => panic!("the request body has already been upgraded"),
+            Inner::Stream(body) => body.poll_trailers().map_err(Into::into),
+            Inner::OnUpgrade(..) => Err(already_upgraded()),
         }
     }
 }
 
 impl Upgrade for RequestBody {
-    type Upgraded = hyper::upgrade::Upgraded;
-    type Error = hyper::Error;
+    type Upgraded = Upgraded;
+    type Error = BoxedStdError;
 
     fn poll_upgrade(&mut self) -> Poll<Self::Upgraded, Self::Error> {
         loop {
             self.0 = match &mut self.0 {
-                Inner::Raw(body) => {
-                    let body = std::mem::replace(body, Default::default());
+                Inner::Stream(body) => {
+                    let body = std::mem::replace(body, hyper::Body::empty());
                     Inner::OnUpgrade(body.on_upgrade())
                 }
-                Inner::OnUpgrade(on_upgrade) => return on_upgrade.poll(),
+                Inner::OnUpgrade(on_upgrade) => {
+                    return on_upgrade
+                        .poll()
+                        .map(|x| x.map(Upgraded))
+                        .map_err(Into::into);
+                }
             };
         }
+    }
+}
+
+#[derive(Debug)]
+pub struct Upgraded(hyper::upgrade::Upgraded);
+
+impl Upgraded {
+    #[inline]
+    pub fn downcast<T>(self) -> Result<(T, Bytes), Self>
+    where
+        T: AsyncRead + AsyncWrite + 'static,
+    {
+        self.0
+            .downcast::<T>()
+            .map(|parts| (parts.io, parts.read_buf))
+            .map_err(Upgraded)
+    }
+}
+
+impl io::Read for Upgraded {
+    #[inline]
+    fn read(&mut self, dst: &mut [u8]) -> io::Result<usize> {
+        self.0.read(dst)
+    }
+}
+
+impl io::Write for Upgraded {
+    #[inline]
+    fn write(&mut self, src: &[u8]) -> io::Result<usize> {
+        self.0.write(src)
+    }
+
+    #[inline]
+    fn flush(&mut self) -> io::Result<()> {
+        self.0.flush()
+    }
+}
+
+impl AsyncRead for Upgraded {
+    #[inline]
+    unsafe fn prepare_uninitialized_buffer(&self, buf: &mut [u8]) -> bool {
+        self.0.prepare_uninitialized_buffer(buf)
+    }
+
+    #[inline]
+    fn read_buf<B: BufMut>(&mut self, buf: &mut B) -> Poll<usize, io::Error> {
+        self.0.read_buf(buf)
+    }
+}
+
+impl AsyncWrite for Upgraded {
+    #[inline]
+    fn write_buf<B: Buf>(&mut self, buf: &mut B) -> Poll<usize, io::Error> {
+        self.0.write_buf(buf)
+    }
+
+    #[inline]
+    fn shutdown(&mut self) -> Poll<(), io::Error> {
+        self.0.shutdown()
     }
 }
 
