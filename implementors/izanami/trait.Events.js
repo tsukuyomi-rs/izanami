7f3195e2452aca10d2f009cd9f0f99602ecc646d
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-11-27 08:38:00 +0000

    Automated deployment: Wed Nov 27 08:38:00 UTC 2019 2f9b8788b010a947b41b7b34bc300769c794478c

--- implementors/izanami/trait.Events.js
+++ implementors/izanami/trait.Events.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["izanami"] = [];
 implementors["izanami_h2"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"izanami/trait.Events.html\" title=\"trait izanami::Events\">Events</a> for <a class=\"struct\" href=\"izanami_h2/struct.Events.html\" title=\"struct izanami_h2::Events\">Events</a>&lt;'a&gt;",synthetic:false,types:["izanami_h2::Events"]},];
 implementors["izanami_hyper"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"izanami/trait.Events.html\" title=\"trait izanami::Events\">Events</a> for <a class=\"struct\" href=\"izanami_hyper/struct.Events.html\" title=\"struct izanami_hyper::Events\">Events</a>&lt;'a&gt;",synthetic:false,types:["izanami_hyper::Events"]},];
 
