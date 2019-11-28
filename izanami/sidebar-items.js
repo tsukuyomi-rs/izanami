eb206887f5695d1a5b47f1590a499e2328c7340d
ubnt-intrepid <ubnt-intrepid@users.noreply.github.com>
2019-11-28 08:18:15 +0000

    Automated deployment: Thu Nov 28 08:18:15 UTC 2019 d848ddd577e4a107b2e73c9909d3a161e0686c9e

--- izanami/sidebar-items.js
+++ izanami/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["App",""],["Events",""]]});
+initSidebarItems({"trait":[["App","A trait that models Web applications."],["Events","Asynchronous object that exchanges the events with the client."]]});
